488fdaba6593ecc832d995f20132fcf64c7b6091
McSilva <microrepar@gmail.com>
2023-10-27 20:35:29 -0300

    feat: adds default project name and alter tag name project directory from {{cookiecutter.project_name}} to {{cookiecutter.project_slug}}

--- {{cookiecutter.project_name}}/entry_point_app.py
+++
@@ -1,2 +0,0 @@
-"""Application's entry point
-"""
---
+++ {{cookiecutter.project_slug}}/entry_point_app.py
@@ -0,0 +1,5 @@
+"""Application's entry point
+Project name: {{cookiecutter.}}
+Author: {{cookiecutter.author}}
+Description: {{cookiecutter.description}}
+"""
