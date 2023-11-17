0949f41307f19219a12344f80020553de2fa60f6
McSilva <microrepar@gmail.com>
2023-11-17 18:37:24 -0300

    refact: refactory [if operator] to verification DATABASE_URL environment variable

--- {{cookiecutter.project_slug}}/config.py
+++ {{cookiecutter.project_slug}}/config.py
@@ -6,17 +6,19 @@ load_dotenv()
 
 
 class Config:
-    DB_DIALECT   = os.getenv('DB_DIALECT')
-    DB_USER      = os.getenv('DB_USER')
-    DB_PASSWORD  = os.getenv('DB_PASSWORD')
-    DB_HOST      = os.getenv('DB_HOST')
-    DB_PORT      = os.getenv('DB_PORT')
-    DB_DATABASE  = os.getenv('DB_DATABASE')
-    DB_SCHEMA    = os.getenv('DB_SCHEMA')
-    DATABASE_URL = os.getenv('DATABASE_URL')
+    DATABASE_URL   = os.getenv('DATABASE_URL')
+    DB_DATABASE    = os.getenv('DB_DATABASE')
+    DB_DIALECT     = os.getenv('DB_DIALECT')
+    DB_HOST        = os.getenv('DB_HOST')
+    DB_PASSWORD    = os.getenv('DB_PASSWORD')
+    DB_PORT        = os.getenv('DB_PORT')
+    DB_SCHEMA      = os.getenv('DB_SCHEMA')
+    DB_USER        = os.getenv('DB_USER')
+
+    FRAMEWORK_NAME = os.getenv('FRAMEWORK_NAME')
 
     if DATABASE_URL is None:
-        if DB_DIALECT in 'postgresql mysql':
+        if DB_DIALECT and DB_DIALECT in 'postgresql mysql':
             DATABASE_URL = f"{DB_DIALECT}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"    
         else:
             DATABASE_URL = f"sqlite:///{Path(__file__).parent}/app.db"
