b422428fc62dd5e1fe2f1c34a83c26aaff06d92f
McSilva <microrepar@gmail.com>
2023-11-26 23:38:43 -0300

    feat: centralizes the configurations in the config.py file and adds the environment variable files as an example

--- {{cookiecutter.project_slug}}/config.py
+++ {{cookiecutter.project_slug}}/config.py
@@ -1,21 +1,45 @@
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
+FRAMEWORK_NAME = os.getenv('FRAMEWORK_NAME')
 
 class Config:
-    DATABASE_URL   = os.getenv('DATABASE_URL')
-    DB_DATABASE    = os.getenv('DB_DATABASE')
-    DB_DIALECT     = os.getenv('DB_DIALECT')
-    DB_HOST        = os.getenv('DB_HOST')
-    DB_PASSWORD    = os.getenv('DB_PASSWORD')
-    DB_PORT        = os.getenv('DB_PORT')
-    DB_SCHEMA      = os.getenv('DB_SCHEMA')
-    DB_USER        = os.getenv('DB_USER')
+    if FRAMEWORK_NAME == 'streamlit':
+        import streamlit as st
+        DATABASE_URL = st.secrets.get('DATABASE_URL')
+        DB_DATABASE  = st.secrets.get('DB_DATABASE')
+        DB_DIALECT   = st.secrets.get('DB_DIALECT')
+        DB_HOST      = st.secrets.get('DB_HOST')
+        DB_PASSWORD  = st.secrets.get('DB_PASSWORD')
+        DB_PORT      = st.secrets.get('DB_PORT')
+        DB_SCHEMA    = st.secrets.get('DB_SCHEMA')
+        DB_USER      = st.secrets.get('DB_USER')
+        
+        HOST         = st.secrets.get('HOST')
+        PORT         = st.secrets.get('PORT')
+        PASSWORD     = st.secrets.get('PASSWORD')
 
-    FRAMEWORK_NAME = os.getenv('FRAMEWORK_NAME')
+        DB_FRAMEWORK = st.secrets.get('DB_FRAMEWORK')
+
+    else:
+        DATABASE_URL = os.getenv('DATABASE_URL')
+        DB_DATABASE  = os.getenv('DB_DATABASE')
+        DB_DIALECT   = os.getenv('DB_DIALECT')
+        DB_HOST      = os.getenv('DB_HOST')
+        DB_PASSWORD  = os.getenv('DB_PASSWORD')
+        DB_PORT      = os.getenv('DB_PORT')
+        DB_SCHEMA    = os.getenv('DB_SCHEMA')
+        DB_USER      = os.getenv('DB_USER')
+
+        HOST         = os.getenv('HOST')
+        PORT         = os.getenv('PORT')
+        PASSWORD     = os.getenv('PASSWORD')
+
+        DB_FRAMEWORK = os.getenv('DB_FRAMEWORK')
 
     if DATABASE_URL is None:
         if DB_DIALECT and DB_DIALECT in 'postgresql mysql':
