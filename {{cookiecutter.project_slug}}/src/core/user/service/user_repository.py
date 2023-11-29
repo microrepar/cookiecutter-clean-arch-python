7c557456181f7fbb0db181692da40f761d7a7799
McSilva <microrepar@gmail.com>
2023-11-29 15:33:24 -0300

    refact: moves abstract methods from UserRepository to Repository

--- {{cookiecutter.project_slug}}/src/core/user/service/user_repository.py
+++ {{cookiecutter.project_slug}}/src/core/user/service/user_repository.py
@@ -1,39 +1,9 @@
 from abc import abstractmethod
 from typing import List, Protocol, runtime_checkable
 
-from src.core.shared.repository import Repository
+from src.core.shared import Repository
 from src.core.user import User
 
 
 @runtime_checkable
-class UserRepository(Repository, Protocol):
-    
-    @abstractmethod
-    def registry(self, entity: User) -> User:
-        """_summary_
-        """
-
-    @abstractmethod
-    def get_all(self, entity: User) -> List[User]:
-        """_summary_
-        """
-
-    @abstractmethod
-    def update(self, entity: User) -> User:
-        """_summary_
-        """
-    
-    @abstractmethod
-    def find_by_field(self, entity: User) -> List[User]:
-        """_summary_
-        """
-    
-    @abstractmethod
-    def get_by_id(self, entity: User) -> User:
-        """_summary_
-        """
-    
-    @abstractmethod
-    def remove(self, entity: User) -> bool:
-        """_summary_
-        """
+class UserRepository(Repository, Protocol):...
