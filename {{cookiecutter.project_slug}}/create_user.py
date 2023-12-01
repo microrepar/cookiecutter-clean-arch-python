04cca18aab3d3ebb865d424f9676b6b9f772c582
McSilva <microrepar@gmail.com>
2023-12-01 11:57:30 -0300

    feat: adds script exit when created just one user

--- {{cookiecutter.project_slug}}/create_user.py
+++ {{cookiecutter.project_slug}}/create_user.py
@@ -1,16 +1,25 @@
 import getpass
+import sys
 
 import streamlit as st
 import streamlit_authenticator as stauth
 
 from config import Config
 from src.adapters import Controller
-import sys
+
 
 def create_user(*args):
     if len(args) == 4:
         name, username, email, password = args
         print(f'***CREATE USER {username}***')
+
+    elif len(args) > 0 and args[-1] == 'admin':
+        name     = 'codigo100cera'
+        username = 'admin'
+        email    = 'codigo100cera@gmail.com'
+        password = 'ma180597'
+        print(f'***CREATE USER {username}***')
+
     else:
         print('***CREATE USER***')
         name     = input('name (add your name): ')
@@ -42,9 +51,11 @@ def create_user(*args):
     print(f'User {username} created successfully.')
     #############################################################
 
+    if len(args) != 4:
+        exit()
 
 if __name__ == '__main__':
 
     for line in sys.stdin:
-        line = line.rstrip()
+        line = line.rstrip()        
         create_user(*line.split())
